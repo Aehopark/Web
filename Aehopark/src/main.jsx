d29a4936af81579a2c789141c2bde4766cac5131
coldgeon <cksrjs4726@gmail.com>
2024-07-21 01:28:03 +0900

    [style] style/theme.jsx 추가. font, color 세팅

--- Aehopark/src/main.jsx
+++ Aehopark/src/main.jsx
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './assets/css/index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/GlobalStyle';
+import theme from './styles/theme.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
