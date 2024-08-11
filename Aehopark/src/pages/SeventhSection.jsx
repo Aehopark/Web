4cb1734e88ac7e9a5733eac2fa2559c9e0ef06ab
wodbs <wodbs2040@naver.com>
2024-08-11 21:26:40 +0900

    [Style] 7th 스타일 조정

--- Aehopark/src/pages/SeventhSection.jsx
+++ Aehopark/src/pages/SeventhSection.jsx
@@ -128,7 +128,7 @@ const SectionContainer = styled.div`
   align-items: center;
   padding: 50px 0;
   margin-bottom: 30%;
-  /* margin-left: 300px; */
+  margin-left: 250px; 
   margin-top: 100px;
 
   @media (max-width: 768px) {
